1f3bd85df9742581296424186c0735c0eaa63e5b
HsuChi_Chen <chenneil90121@gmail.com>
2021-08-23 03:04:10 +0800

    lec134

--- hw34.cpp
+++ hw34.cpp
@@ -1,51 +1,34 @@
+//影片P135
+//多態
 #include <iostream>
 using namespace std;
 
-class base{
+class animal{
 public:
-    base(){
-        m_A = 100;
-        m_B = 500;
+    virtual void speak(){
+        cout << "animal is speaking" << endl;
     }
-    void fun(){
-        cout << "fun() in base" << endl;
-    }
-
-    void fun(int a){
-        cout << "fun(int) in base" << endl;
-    }
-
-    int m_A;
-    int m_B;
 };
 
-class son :public base{
+class cat :public animal{
 public:
-    son(){
-        m_A = 200;
+    void speak(){
+        cout << "cat is speaking" << endl;
     }
-    void fun(){
-        cout << "fun() in son" << endl;
-    }
-
-    int m_A;
 };
 
-void test01(){
-    son s;
-    cout << s.m_A << endl;
-    cout << s.base::m_A << endl;
-    cout << s.m_B << endl;
+//地址早綁定，在編譯階段確定函數地址
+//如果想執行讓貓說話，那麼這個函數地址就不能提前綁定，需要在運行階段進行綁定
+void do_speak(animal &animal){ //animal &animal = cat 父類引用指向子類對象
+    animal.speak();
 }
 
-void test02(){
-    son s;
-    s.base::fun();
-    s.base::fun(10);
+void test01(){
+    cat cat01;
+    do_speak(cat01);
 }
 
 int main(){
-    // test01();
-    test02();
+    test01();
     return 0;
 }
